89c349312f790aa6f6252deada000372e018ed5b
Sebastián Córdoba <94708762+sebastiancordoba@users.noreply.github.com>
2026-02-25 20:59:46 -0600

    chore: enable Homebrew installation via Formula

--- HomebrewFormula/zathura-web.rb
+++ HomebrewFormula/zathura-web.rb
@@ -2,7 +2,7 @@ class ZathuraWeb < Formula
   desc "A minimalist, keyboard-driven PDF and EPUB reader"
   homepage "https://github.com/sebastiancordoba/web-pdf"
   url "https://github.com/sebastiancordoba/web-pdf/releases/download/v0.1.0/ZathuraWeb.app.tar.gz"
-  sha256 "REPLACE_ME_WITH_SHA" # You will need to calculate this
+  sha256 "d3296ad397ea0acd3ba044581334b95b6c61ea732c4795b674ed441912b9eaad"
   version "0.1.0"
 
   def install
